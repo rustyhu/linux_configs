e0cf8adbbd6c315dc9b61a2448203fa6f0cbaf81
rustyhu@hotmail.com <rustyhu@hotmail.com>
2015-07-12 14:34:42 +0800

    Arch and Debian bashrc was bulit.

--- auto_push.sh
+++ auto_push.sh
@@ -1,6 +1,6 @@
-
+#!/bin/bash
 # push my commits automatically
 
-# 'git add ...' needs to be operate manually now.
+# 'git add ...' still needs to be operate manually.
 git commit -m "$1"
 git push origin master
