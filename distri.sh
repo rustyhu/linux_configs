58db9233aa6b0d9b42cd291d0eaa1872d427bff2
rustyhu@hotmail.com <rustyhu@hotmail.com>
2016-02-02 22:34:10 +0800

    emacs config moved outside to build seperate repo.

--- distri.sh
+++ distri.sh
@@ -4,11 +4,8 @@
 # The feature of prompting user for softwares' installation directories will be added later.
 
 # Source: git repo here
-GIT_DIR_E=emacs
 GIT_DIR_V=vim
 # By default target directory is usually ${HOME}.
-TARGET_DIR_E=$HOME
 TARGET_DIR_V=$HOME
 
-cp "${GIT_DIR_E}/.emacs" "${TARGET_DIR_E}/"
 cp "${GIT_DIR_V}/.vimrc" "${TARGET_DIR_E}/"
